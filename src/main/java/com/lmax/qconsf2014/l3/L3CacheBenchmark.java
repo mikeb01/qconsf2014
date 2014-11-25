ebb4f21505d2b793a0b3dfa4cb6f1807b642f8d5
Michael Barker <michael.barker@lmax.com>
2014-11-25 13:43:05 +1300

    Update L3 performance test.

--- src/main/java/com/lmax/qconsf2014/l3/L3CacheBenchmark.java
+++ src/main/java/com/lmax/qconsf2014/l3/L3CacheBenchmark.java
@@ -8,34 +8,54 @@ package com.lmax.qconsf2014.l3;
 import java.util.Random;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class L3CacheBenchmark
 {
+    @State(Scope.Benchmark)
+    public static class P
+    {
+        @Param({
+             "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9", "10", "11", "12",
+            "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
+        })
+        public int size;
+    }
+
     @State(Scope.Thread)
     public static class ThreadData
     {
-        long[] values = new long[1 << 19];
-        int[] indexes = new int[values.length];
-        
-        public ThreadData()
+        int[] values;
+        int[] indexes;
+
+        @Setup
+        public void setup(P p)
         {
+            values = new int[1 << p.size];
+            indexes = new int[values.length];
+
             for (int i = 0; i < values.length; i++)
             {
                 values[i] = i;
                 indexes[i] = i;
             }
-            
+
             shuffleArray(indexes);
         }
     }
-    
+
     @Benchmark
     public void processLongs(Blackhole bh, ThreadData data)
     {
-        for (int i : data.indexes)
+        for (final int i : data.indexes)
         {
             bh.consume(data.values[i]);
         }
@@ -43,14 +63,20 @@ public class L3CacheBenchmark
 
     private static void shuffleArray(int[] ar)
     {
-        Random rnd = new Random();
+        final Random rnd = new Random();
         for (int i = ar.length - 1; i > 0; i--)
         {
-            int index = rnd.nextInt(i + 1);
+            final int index = rnd.nextInt(i + 1);
             // Simple swap
-            int a = ar[index];
+            final int a = ar[index];
             ar[index] = ar[i];
             ar[i] = a;
         }
+    }
+
+    public static void main(String[] args) throws RunnerException
+    {
+        final Options opt = new OptionsBuilder().include(L3CacheBenchmark.class.getSimpleName()).build();
+        new Runner(opt).run();
     }
 }
